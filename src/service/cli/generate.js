243e655599a5bfb7424303312777c15b20b33073
ella <ellasidorova@yandex.ru>
2022-05-26 16:16:56 +0300

    create files

---
+++ src/service/cli/generate.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const {
+    getRandomInt,
+    shuffle,
+    createDate,
+} = require(`../../utils`);
+
+const {
+    TITLES,
+    SENTENCES,
+    CATEGORY,
+    ArrayElements,
+} = require(`../../constants`);
+
+module.exports = {
+    name: `--generate`,
+    init() {
+    }
+};
